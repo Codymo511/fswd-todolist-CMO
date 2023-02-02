fee7f707612e4e1d21e0f26135feb45c78425e2e
Codymo511 <55954712+Codymo511@users.noreply.github.com>
2023-02-01 19:55:07 -0500

    initial commit

---
+++ app/javascript/src/index.js
@@ -0,0 +1,37 @@
+import $ from 'jquery';
+
+import {
+  indexTasks,
+  postTask,
+  deleteTask
+} from "./requests.js";
+
+
+indexTasks(function (response) {
+  var htmlString = response.tasks.map(function(task) {
+    return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.id + "'> \
+      " + task.content + "  \
+      </div><button class='delete-task' data-id='" + task.id + "'> Delete </button>";
+  });
+
+  $("#tasks").html(htmlString);
+});
+
+$(document).on('submit','#create-task',function(event){
+    event.preventDefault();
+    var content = $('#new-task-content').val();
+    postTask(content)
+    indexTasks;
+  })
+  
+  $(document).on('click','.delete-task',function(){
+    var id = $(this).data('id');
+      deleteTask(id)
+      indexTasks;
+  })
+
+
+
+ 
+
+
